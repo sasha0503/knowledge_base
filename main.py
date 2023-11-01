08d5f2a8a2b1085916d8ed96dd42f9f5c5f78cf2
sasha <o.l@smoothr.de>
2023-11-01 15:49:54 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from rdflib import URIRef, BNode, Literal, Namespace, Graph
+from rdflib.namespace import FOAF, DCTERMS, XSD, RDF, SDO
+
+"""knowledge base with RDFLib"""
+
+# create subjects and objects
+mona_lisa = URIRef('http://www.wikidata.org/entity/Q12418')
+davinci = URIRef('http://dbpedia.org/resource/Leonardo_da_Vinci')
+lajoconde = URIRef('http://data.europeana.eu/item/04802/243FA8618938F4117025F17A8B813C5F9AA4D619')
+
+# use namespace to shorten URIs
+EX = Namespace('http://example.org/')
+bob = EX['Bob']
+alice = EX['Alice']
+
+birth_date = Literal("1990-07-04", datatype=XSD['date'])
+title = Literal('Mona Lisa', lang='en')
+
+g = Graph()
+
+# Bind prefix to namespace
+g.bind('ex', EX)
+g.bind('foaf', FOAF)
+g.bind('schema', SDO)
+g.bind('dcterms', DCTERMS)
+
+# add triples
+g.add((bob, RDF.type, FOAF.Person))
+g.add((bob, FOAF.knows, alice))
+g.add((bob, FOAF['topic_interest'], mona_lisa))
+g.add((bob, SDO['birthDate'], birth_date))
+g.add((mona_lisa, DCTERMS['creator'], davinci))
+g.add((mona_lisa, DCTERMS['title'], title))
+g.add((lajoconde, DCTERMS['subject'], mona_lisa))
+
+# print graph
+print(g.serialize(format='ttl'))
+
+# Remove triples from graph
+g.remove((mona_lisa, None, None))
+
+# print graph
+print(g.serialize(format='ttl'))
