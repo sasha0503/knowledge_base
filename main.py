aa62e27f57cf8ed07ee0a7b704c5b664551b96df
sasha <o.l@smoothr.de>
2023-11-02 09:28:21 +0200

    update

--- main.py
+++ main.py
@@ -1,47 +1,109 @@
-from rdflib import URIRef, BNode, Literal, Namespace, Graph
-from rdflib.namespace import FOAF, DCTERMS, XSD, RDF, SDO
+# Car types and their conditions
+car_conditions = {
+    'compact': {
+        'fuel_efficiency': 'high',
+        'space': 'compact',
+        'power': 'low'
+    },
+    'sedan': {
+        'fuel_efficiency': 'moderate',
+        'space': 'moderate',
+        'power': 'moderate'
+    },
+    'SUV': {
+        'fuel_efficiency': 'low',
+        'space': 'high',
+        'power': 'high'
+    },
+    'sports': {
+        'fuel_efficiency': 'moderate',
+        'space': 'low',
+        'power': 'very_high'
+    },
+    'hybrid': {
+        'fuel_efficiency': 'very_high',
+        'space': 'moderate',
+        'power': 'moderate'
+    }
+}
 
-"""knowledge base with RDFLib"""
+# Questions
+questions = {
+    'fuel_efficiency': 'How important is fuel efficiency to you?',
+    'space': 'How much space do you need?',
+    'power': 'How important is high power for you?'
+}
 
-# create subjects and objects
-mona_lisa = URIRef('http://www.wikidata.org/entity/Q12418')
-davinci = URIRef('http://dbpedia.org/resource/Leonardo_da_Vinci')
-lajoconde = URIRef('http://data.europeana.eu/item/04802/243FA8618938F4117025F17A8B813C5F9AA4D619')
+# Answers
+answers = {
+    'low': 'Low',
+    'moderate': 'Moderate',
+    'very_high': 'Very High',
+    'compact': 'Compact',
+    'high': 'High'
+}
 
-# use namespace to shorten URIs
-EX = Namespace('http://example.org/')
-bob = EX['Bob']
-alice = EX['Alice']
+cars = {
+    "compact": {
+        "description": "Compact Car\nSmall and fuel-efficient, perfect for city driving."
+    },
+    "sedan": {
+        "description": "Sedan\nA practical choice for families with a balance of space, fuel efficiency, and power."
+    },
+    "SUV": {
+        "description": "SUV\nSpacious and powerful, ideal for off-road adventures and large families."
+    },
+    "sports": {
+        "description": "Sports Car\nHigh-performance and stylish, best for those who love speed and handling."
+    },
+    "hybrid": {
+        "description": "Hybrid Car\nEnvironmentally friendly and fuel-efficient, combining electric and gasoline power."
+    }
+}
 
-birth_date = Literal("1990-07-04", datatype=XSD['date'])
-title = Literal('Mona Lisa', lang='en')
 
-g = Graph()
+def introduction():
+    print('What type of car best suits your needs?')
+    print()
 
-# Bind prefix to namespace
-g.bind('ex', EX)
-g.bind('foaf', FOAF)
-g.bind('schema', SDO)
-g.bind('dcterms', DCTERMS)
 
-# add triples
-g.add((bob, RDF.type, FOAF.Person))
-g.add((bob, FOAF.knows, alice))
-g.add((bob, FOAF['topic_interest'], mona_lisa))
-g.add((bob, SDO['birthDate'], birth_date))
-g.add((mona_lisa, DCTERMS['creator'], davinci))
-g.add((mona_lisa, DCTERMS['title'], title))
-g.add((lajoconde, DCTERMS['subject'], mona_lisa))
+# Ask a question
+def ask(question, options):
+    print(questions[question])
+    for i, option in enumerate(options):
+        print(f'{i + 1}. {answers[option]}')
+    answer = input()
+    while not answer.isdigit() or int(answer) < 1 or int(answer) > len(options):
+        print('Invalid answer, please try again')
+        answer = input()
+    return options[int(answer) - 1]
 
-# print graph
-print(g.serialize(format='ttl'))
 
-# Replace Literal value
-g.set((bob, SDO['birthDate'], Literal('1990-01-01', datatype=XSD.date)))
-g.set((mona_lisa, DCTERMS['title'], Literal('La Joconde', lang='fr')))
+def find_car():
+    fuel_efficiency = ask('fuel_efficiency', ['low', 'moderate', 'high', 'very_high'])
+    space = ask('space', ['compact', 'moderate', 'high'])
+    power = ask('power', ['low', 'moderate', 'high', 'very_high'])
 
-# Remove triples from graph
-g.remove((mona_lisa, None, None))
+    car2score = {}
+    for car, conditions in car_conditions.items():
+        score = 0
+        if conditions['fuel_efficiency'] == fuel_efficiency:
+            score += 1
+        if conditions['space'] == space:
+            score += 1
+        if conditions['power'] == power:
+            score += 1
+        car2score[car] = score
 
-# print graph
-print(g.serialize(format='ttl'))
+    best_score = max(car2score.values())
+    return [car for car, score in car2score.items() if score == best_score][0]
+
+
+def main():
+    introduction()
+    car = find_car()
+    print(f"You should consider a {car}!")
+
+
+if __name__ == "__main__":
+    main()
